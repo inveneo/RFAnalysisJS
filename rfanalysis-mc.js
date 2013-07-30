30f4e4a8159d01a08405adfffa23ceba95415123
mattcrum <inveneo@Matt-Work-3.local>
2013-07-29 18:43:23 -0700

    Fixed the ellipse finally.

--- rfanalysis-mc.js
+++ rfanalysis-mc.js
@@ -36,15 +36,15 @@ function Link(x1,y1,x2,y2,frequency,t1,t2,fresnelZoneNumber)
 Link.prototype.getFresnelZoneHeightAtPoint = function(point)
 {
   var dist1M = point;
-  console.log("dist1M = " + dist1M);
+  //console.log("dist1M = " + dist1M);
   var dist2M = ((this.linkLength * 1000 ) - dist1M);
   //var dist2M = ((this.linkLength) - dist1M);
   //console.log("dist1M + dist2M = " +  (dist1M + dist2M));
-  console.log("dist2M = " + dist2M);
-  console.log("Math.sqrt(( "+this.fresnelZoneNumber+" * "+this.lambda+ " * " + dist1M + " * " + dist2M+' )/( ' +dist1M +" + " + dist2M+"))");
+  //console.log("dist2M = " + dist2M);
+  //console.log("Math.sqrt(( "+this.fresnelZoneNumber+" * "+this.lambda+ " * " + dist1M + " * " + dist2M+' )/( ' +dist1M +" + " + dist2M+"))");
   var fresnelZone = Math.sqrt((this.fresnelZoneNumber*this.lambda*dist1M*dist2M)/(dist1M+dist2M));
-  console.log("fresnelZone = " + fresnelZone);
-  console.log("Height of Fresnel Zone at "+dist1M+" meters is "+fresnelZone.toFixed(2));
+  //console.log("fresnelZone = " + fresnelZone);
+  //console.log("Height of Fresnel Zone at "+dist1M+" meters is "+fresnelZone.toFixed(2));
   return fresnelZone.toFixed(2);
 };
 
@@ -93,10 +93,31 @@ Link.prototype.elevationAnalysis = function (elevationObj)
   
   var lineOfSite1 = elevationArray[0] + this.t1;
   var lineOfSite2 = elevationArray[elevationArray.length-1]+this.t2;
+  var lineOfSiteArrayX = [];
+  var lineOfSiteArrayY = [];
+  var lineOfSiteChangeY = 0;
+  if(lineOfSite2>=lineOfSite1){
+  	lineOfSiteChangeY=(lineOfSite2-lineOfSite1)/elevationArray.length;
+  }
+  else{
+	  lineOfSiteChangeY=(lineOfSite1-lineOfSite2)/elevationArray.length;
+  }
+  console.log("lineOfSiteChangeY = " + lineOfSiteChangeY);
+  for(i=0; i<elevationArray.length;i++)
+  {
+	  lineOfSiteArrayX[i]= i;
+	  lineOfSiteArrayY[i]= lineOfSite1+ lineOfSiteChangeY*i;
+  }
+  console.log(lineOfSiteArrayX);
   
-  var lines = elevationGraphPaper.linechart(20, 20, 1200, 300, [arrayX,[arrayX[0],arrayX[arrayX.length-1]]], [elevationArray,[lineOfSite1,lineOfSite2]], { nostroke: false, symbol: "circle", axis: "0 0 1 1", smooth: true }).hoverColumn(function () {
+  var heightReferenceLineMin = Math.min.apply(Math,elevationArray);
+  var heightReferenceLineMax = getHeightOfGraphInMeters(elevationArray)+heightReferenceLineMin;
+  
+  console.log(elevationArray);
+  console.log("heightReferenceLineMax="+heightReferenceLineMax);
+  console.log("heightReferenceLineMin="+heightReferenceLineMin);
+  var lines = elevationGraphPaper.linechart(20, 20, 1200, 300, [arrayX,lineOfSiteArrayX,[0,0]], [elevationArray,lineOfSiteArrayY,[heightReferenceLineMin,heightReferenceLineMax]], { nostroke: false, symbol: "circle", axis: "0 0 1 1", smooth: true }).hoverColumn(function () {
       this.tags = elevationGraphPaper.set();
-
       for (var i = 0, ii = this.y.length; i < ii; i++) {
           this.tags.push(elevationGraphPaper.tag(this.x, this.y[i], this.values[i]+" | Lat: "+elevationLatArray[this.axis]+" | Lng: "+elevationLngArray[this.axis], 10, 10).insertBefore(this).attr([{ fill: "#fff" }, { fill: this.symbols[i].attr("fill") }]));
       }
@@ -109,9 +130,9 @@ Link.prototype.elevationAnalysis = function (elevationObj)
   console.log(beginPoint);
   console.log(endPoint);
   
-  lines.symbols.attr({ r: 3 });
+  lines.symbols.attr({ r: 1 });
   //lines.lines[1].animate({"stroke-width": 4}, 1000);
-  lines.lines[1].attr({"stroke": 0, fill:"#ccc"});
+  lines.lines[1].attr({"stroke": 0});
   // lines.symbols[0].attr({stroke: "#fff"});
   
   //create red dot at first and last point
@@ -122,12 +143,21 @@ Link.prototype.elevationAnalysis = function (elevationObj)
   var lineOfSitePointBegin = lineOfSiteLine.getPointAtLength(0,0);
   var lineOfSitePointEnd =  lineOfSiteLine.getPointAtLength(lineOfSiteLine.getTotalLength());
   var lineOfSiteDrawingLength = lineOfSiteLine.getTotalLength();
+  
+  var lineOfSiteDrawingHeight = 0;
+  
+	refBbox = lines.lines[0].getBBox();
+	console.log("refHeight= "+refBbox.height);
+	
   var fresnelAngle = Raphael.angle(lineOfSitePointBegin.x,lineOfSitePointBegin.y,lineOfSitePointEnd.x,lineOfSitePointEnd.y);
-  var fresnelPath = this.getFresnelPath(lineOfSiteDrawingLength,lineOfSitePointBegin,lineOfSitePointEnd);
-  var fresnelShape = elevationGraphPaper.path(fresnelPath);
-  //var fresnelShape = elevationGraphPaper.path("M 0 0 L 0,0L20,0L,40,100z");
+  
+  //!!!! NOTE:  Object.keys(elevationObj).length MAY ONLY WORK IN Chrome, IE 9+, FF 4+, or Safari 5+ !!!!!
+  var resolution = Object.keys(elevationObj).length;
+  console.log("resolution is " + resolution);
+  var fresnelValues = this.getFresnelValues(lineOfSiteDrawingLength,refBbox.height,lineOfSitePointBegin,lineOfSitePointEnd,resolution,elevationArray,lineOfSiteArrayY);
+  
+  var fresnelShape = elevationGraphPaper.path(fresnelValues.path);
   fresnelShape.attr({fill:"#ff6633", opacity:0.5, stroke:0});
-  //fresnelShape.rotate(fresnelAngle);
   var shapeBeginPoint = fresnelShape.getPointAtLength(0);
   var translateX=shapeBeginPoint.x-lineOfSitePointBegin.x;
   var translateY = shapeBeginPoint.y-lineOfSitePointBegin.y;
@@ -135,44 +165,106 @@ Link.prototype.elevationAnalysis = function (elevationObj)
   //fresnelShape.transform("t"+translateX+","+translateY+"r"+fresnelAngle);
   fresnelShape.transform("r"+(fresnelAngle+180)+","+shapeBeginPoint.x+","+shapeBeginPoint.y+"T"+lineOfSitePointBegin.x+","+lineOfSitePointBegin.y);
   
+  console.log("path intersection follows:");
+  var intersections = Raphael.pathIntersection(lines.lines[0],fresnelValues.path);
+	 
+	intersections.forEach(function(co) {
+	  elevationGraphPaper.circle(co.x, co.y, 10).attr({stroke: 'none', fill: '#333'})
+	})
+	  
+  lines.hoverColumn(function(){
+  	/* !!!!! THIS IS NEED TO BE REMOVED SO THAT NO JQUERY IS REQUIRED IN THE LIBRARY  !!!!!!!!!!!  */
+  	var currentFresnelZone = fresnelValues.data[this.axis].fresnel;
+	  $("#fresnel-zone-data").html("Lat: "+elevationLatArray[this.axis]+" | Lng: "+elevationLngArray[this.axis]+" <br /> Fresnel Value at "+((fresnelValues.data[this.axis].distance)*.001).toFixed(2)+"km is "+currentFresnelZone + "m <br /> Percent of fresnel intrusion is " + fresnelValues.data[this.axis].percentIntruded);
+  });
+  
 }
 
-//Returns SVG string
-Link.prototype.getFresnelPath = function (lineOfSiteDrawingLength,beginPoint,endPoint) 
+
+/*
+Returns an object containing and svg path at attribute "path, and values of "length" and "fresnel" calc for each point
+returned object: ObjectName: {path: <SVGPATH>, 0: {distance: <DIST>, fresnel: <FRESNEL_LENGTH>, 1: {distance: <DIST>, fresnel: <FRESNEL_LENGTH>, ...etc
+*/
+Link.prototype.getFresnelValues = function (lineOfSiteDrawingLength,lineOfSiteDrawingHeight,beginPoint,endPoint,resolution,elevationArray,lineOfSiteArray)
 {
-	//var pathString = "M " +beginPoint.x + " " +beginPoint.y + " ";
 	var pathString = "M 0 0 ";
-	var numberOfSegments = 20;
+	
+	/*
+var lineOfSiteHeightM = 0;
+	if(elevationArray[0] < elevationArray[elevationArray.length-1])
+	{
+		lineOfSiteHeightM = elevationArray[elevationArray.length-1] - elevationArray[0];
+	}
+	else
+	{
+		lineOfSiteHeightM =  elevationArray[0] - elevationArray[elevationArray.length-1];
+	}
+*/
+	var heightOfGraphM = Math.max.apply(Math,elevationArray);
+	//console.log("lineOfSiteHeightM = " + lineOfSiteHeightM);
+	var fresnelValObj = {};
+	fresnelValObj.data = {};
+	var numberOfSegments = resolution;
 	var lengthPerSegmentD = lineOfSiteDrawingLength/numberOfSegments;
 	var linkLengthM = this.linkLength*1000;
 	var lengthPerSegmentM = lengthPerSegmentD*linkLengthM/lineOfSiteDrawingLength;
-	console.log("LengthPerSegmentM = " + lengthPerSegmentM);
-	
-	console.log("Fresnel zone at halfway point = " + this.getFresnelZoneHeightAtPoint(this.linkLength*1000/2));
+	//console.log("elevationArrayRangeM = " + elevationArrayRangeM);
 	for(i=0; i<numberOfSegments; i++)
 	{
-		
 		var xChangeDrawing = lengthPerSegmentD*i;
 		var xChangeM = lengthPerSegmentM*i;
 		var yChangeM = this.getFresnelZoneHeightAtPoint(xChangeM);
-		var yChangeDrawing = yChangeM*lineOfSiteDrawingLength/linkLengthM;
+		var yChangeDrawing = yChangeM * lineOfSiteDrawingHeight/heightOfGraphM;
+		fresnelValObj.data[i] = {};
+		fresnelValObj.data[i].fresnel=yChangeM;
+		fresnelValObj.data[i].distance=xChangeM;
+		fresnelValObj.data[i].percentIntruded=getFresnelClearance(yChangeM,lineOfSiteArray[i],elevationArray[i]);
+		//fresnelValObj.data[i].percentClear= getPercentClear(losLine;
 		pathString += "L "+ (xChangeDrawing) + " " + (yChangeDrawing)+" " ;
 	}
 	
 	//to get mirror y values to draw
 	for(i=numberOfSegments; i>=0; i--)
 	{
+		var j = numberOfSegments*2-i;
 		var xChangeDrawing = lengthPerSegmentD*i;
 		var xChangeM = lengthPerSegmentM*i;
 		var yChangeM = this.getFresnelZoneHeightAtPoint(xChangeM);
-		var yChangeDrawing = yChangeM*lineOfSiteDrawingLength/linkLengthM;
+		var yChangeDrawing = yChangeM*lineOfSiteDrawingHeight/heightOfGraphM;
+		/*
+fresnelValObj.data[j] = {};
+		fresnelValObj.data[j].fresnel=yChangeM;
+		fresnelValObj.data[j].distance=xChangeM;
+		fresnelValObj.data[j].percentIntruded=getFresnelClearance(yChangeM,(lineOfSiteArray[i]),fresnelValObj.data[j].distance);
+		//console.log("yChangeM=" + yChangeM + " | lineOfSiteArray["+i+"]=" + lineOfSiteArray[i] + " | fresnelValObj.data[j].distance["+j+"]="+fresnelValObj.data[j].distance);
+*/
 		pathString += "L " + (xChangeDrawing)+ " " + "-"+(yChangeDrawing)+" " ;
 	}
 	
-
 	pathString +=" Z";
-	console.log(pathString);
-	return pathString;
+	fresnelValObj.path=pathString;
+	console.log(fresnelValObj);
+	return fresnelValObj;
+}
+
+function getHeightOfGraphInMeters(elevationArray)
+{
+	return ((Math.max.apply(Math,elevationArray)) - (Math.min.apply(Math,elevationArray)));
+}
+
+function getFresnelClearance(fresnel,losPoint,elevPoint)
+{
+	var fresnelRadius = fresnel/2;
+	var fresnelPercent = 0; 
+	console.log("fresnel=" + fresnel + " | losPoint=" + losPoint + " | elevPoint="+elevPoint);
+	if (elevPoint > (losPoint-fresnelRadius))
+	{
+		//how many meters over line of sight point
+		var a = elevPoint-(losPoint-fresnelRadius);
+		 
+		fresnelPercent = a/fresnel*100;
+	}
+	return fresnelPercent;
 }
 
 Link.prototype.setLambda = function(freq)
