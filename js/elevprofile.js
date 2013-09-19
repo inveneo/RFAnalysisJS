079c53870260780def73b2f678d8b1ce36dd99ce
mattcrum <inveneo@mattwork.local>
2013-09-19 16:27:57 -0700

    removing files i dont need

--- js/elevprofile.js
+++
@@ -1,9 +0,0 @@
-Syntax error on line 159, column 181: unexpected '\n' (\u000A)
-156 :   console.log elevationArray
-157 :   console.log "heightReferenceLineMax=" + heightReferenceLineMax
-158 :   console.log "heightReferenceLineMin=" + heightReferenceLineMin
-159 :   lines = elevationGraphPaper.linechart(20, 20, 1200, 300, [arrayX, lineOfSiteArrayX, [0, 0]], [elevationArray, lineOfSiteArrayY, [heightReferenceLineMin, heightReferenceLineMax]],
-^^^ :~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^
-160 :     nostroke: false
-161 :     symbol: "circle"
-162 :     axis: "0 0 1 1"
